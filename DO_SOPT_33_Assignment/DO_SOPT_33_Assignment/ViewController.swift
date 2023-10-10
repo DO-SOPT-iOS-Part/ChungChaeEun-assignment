aa431bf2d1456a46a0883e2ee2dca1e8336eb33a
chaentopia <cce523@hanmail.net>
2023-10-10 15:57:06 +0900

    [Chore] Create Project

---
+++ DO_SOPT_33_Assignment/DO_SOPT_33_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DO_SOPT_33_Assignment
+//
+//  Created by 정채은 on 10/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
