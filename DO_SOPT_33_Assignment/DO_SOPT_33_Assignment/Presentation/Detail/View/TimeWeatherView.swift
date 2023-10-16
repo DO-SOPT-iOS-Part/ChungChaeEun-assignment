17142071d481eb3dd3af6796e4e1372f4a14e1d8
chaentopia <cce523@hanmail.net>
2023-10-16 22:08:28 +0900

    Feat [#1] 디테일뷰 스크롤뷰 구현

---
+++ DO_SOPT_33_Assignment/DO_SOPT_33_Assignment/Presentation/Detail/View/TimeWeatherView.swift
@@ -0,0 +1,105 @@
+//
+//  TimeWeatherView.swift
+//  DO_SOPT_33_Assignment
+//
+//  Created by 정채은 on 10/16/23.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+enum Weather {
+    case cloudyNight
+    case heavyRain
+    case rain
+    case rainyDay
+    case thunder
+}
+
+class TimeWeatherView: UIView {
+    
+    let timeLabel = UILabel()
+    let weatherImageView = UIImageView()
+    let tempLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(time: String = "", state: Weather, temp: String = "˚") {
+        super.init(frame: CGRect())
+        setViewState(state: state)
+        timeLabel.text = time
+        tempLabel.text = temp + "˚"
+        setUI()
+    }
+    
+    func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    func setStyle() {
+        
+        timeLabel.do {
+            $0.textColor = .white
+            $0.font = UIFont(name: "SFProDisplay-Regular", size: 17)
+        }
+        
+        tempLabel.do {
+            $0.textColor = .white
+            $0.font = UIFont(name: "SFProDisplay-Medium", size: 22)
+        }
+    }
+    
+    func setLayout() {
+        
+        self.addSubViews(timeLabel,
+                         weatherImageView,
+                         tempLabel)
+        
+        self.snp.makeConstraints {
+            $0.height.equalTo(122)
+            $0.width.equalTo(44)
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+        }
+        
+        weatherImageView.snp.makeConstraints {
+            $0.top.equalTo(timeLabel.snp.bottom).offset(14)
+            $0.width.height.equalTo(44)
+            $0.centerX.equalToSuperview()
+        }
+        
+        tempLabel.snp.makeConstraints {
+            $0.top.equalTo(weatherImageView.snp.bottom).offset(14)
+            $0.centerX.equalToSuperview()
+        }
+    }
+    
+    func setViewState(state: Weather) {
+        switch state {
+        case .cloudyNight:
+            weatherImageView.image = UIImage(named: "icCloudyNight")
+        case .heavyRain:
+            weatherImageView.image = UIImage(named: "icHeavyRain")
+        case .rain:
+            weatherImageView.image = UIImage(named: "icRain")
+        case .rainyDay:
+            weatherImageView.image = UIImage(named: "icRainyDay")
+        case .thunder:
+            weatherImageView.image = UIImage(named: "icThunder")
+        }
+    }
+}
--- DO_SOPT_33_Assignment/DO_SOPT_33_Assignment/Presentation/Detail/ViewController/DetailViewController.swift
+++ DO_SOPT_33_Assignment/DO_SOPT_33_Assignment/Presentation/Detail/ViewController/DetailViewController.swift
@@ -22,6 +22,18 @@ final class DetailViewController: UIViewController {
     let descriptionLabel = UILabel()
     let separateLineView = UIView()
     
+    let weatherScrollView = UIScrollView()
+    let nowWeatherView = TimeWeatherView(time: "Now", state: .cloudyNight, temp: "21")
+    let afterOneHourWeatherView = TimeWeatherView(time: "10시", state: .rain, temp: "21")
+    let afterTwoHourWeatherView = TimeWeatherView(time: "11시", state: .heavyRain, temp: "19")
+    let afterThreeHourWeatherView = TimeWeatherView(time: "12시", state: .thunder, temp: "19")
+    let afterFourHourWeatherView = TimeWeatherView(time: "13시", state: .rainyDay, temp: "19")
+    let afterFiveHourWeatherView = TimeWeatherView(time: "14시", state: .rainyDay, temp: "20")
+    let afterSixHourWeatherView = TimeWeatherView(time: "15시", state: .rain, temp: "22")
+    let afterSevenHourWeatherView = TimeWeatherView(time: "16시", state: .rain, temp: "21")
+    let afterEightHourWeatherView = TimeWeatherView(time: "17시", state: .rainyDay, temp: "21")
+    let afterNineHourWeatherView = TimeWeatherView(time: "18시", state: .cloudyNight, temp: "19")
+    
     let tabBarView = UIView()
     let mapButton = UIButton()
     let listButton = UIButton()
@@ -69,6 +81,30 @@ final class DetailViewController: UIViewController {
             $0.font = UIFont(name: "SFProDisplay-Medium", size: 20)
             $0.textColor = .white
         }
+        
+        cardView.do {
+            $0.backgroundColor = UIColor(white: 1, alpha: 0.006)
+            $0.layer.cornerRadius = 15
+            $0.clipsToBounds = true
+            $0.layer.borderWidth = 0.5
+            $0.layer.borderColor = .init(red: 0 / 255, green: 0 / 255, blue: 0 / 255, alpha: 0.25)
+        }
+        
+        descriptionLabel.do {
+            $0.text = "08:00~09:00에 강우 상태가, 18:00에 한때 흐린 상태가 예상됩니다."
+            $0.numberOfLines = 2
+            $0.lineBreakMode = .byCharWrapping
+            $0.font = UIFont(name: "SFProDisplay-Regular", size: 18)
+            $0.textColor = .white
+        }
+        
+        separateLineView.do {
+            $0.backgroundColor = UIColor(white: 1, alpha: 0.25)
+        }
+        
+        weatherScrollView.do {
+            $0.showsHorizontalScrollIndicator = false
+        }
     }
     
     private func setLayout() {
@@ -76,7 +112,23 @@ final class DetailViewController: UIViewController {
                               localLabel,
                               tempLabel,
                               weatherLabel,
-                              maxMinTempLabel)
+                              maxMinTempLabel,
+                              cardView)
+        
+        cardView.addSubViews(descriptionLabel,
+                             separateLineView,
+                             weatherScrollView)
+        
+        weatherScrollView.addSubViews(nowWeatherView,
+                                      afterOneHourWeatherView,
+                                      afterTwoHourWeatherView,
+                                      afterThreeHourWeatherView,
+                                      afterFourHourWeatherView,
+                                      afterFiveHourWeatherView,
+                                      afterSixHourWeatherView,
+                                      afterSevenHourWeatherView,
+                                      afterEightHourWeatherView,
+                                      afterNineHourWeatherView)
         
         backgroundImageView.snp.makeConstraints {
             $0.edges.equalToSuperview()
@@ -102,7 +154,80 @@ final class DetailViewController: UIViewController {
             $0.centerX.equalToSuperview()
         }
         
+        cardView.snp.makeConstraints {
+            $0.top.equalTo(maxMinTempLabel.snp.bottom).offset(44)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(212)
+            $0.centerX.equalToSuperview()
+        }
         
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(10)
+            $0.leading.trailing.equalToSuperview().inset(15)
+        }
         
+        separateLineView.snp.makeConstraints {
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(11)
+            $0.leading.equalToSuperview().inset(14)
+            $0.trailing.equalToSuperview()
+            $0.height.equalTo(0.2)
+        }
+        
+        weatherScrollView.snp.makeConstraints {
+            $0.top.equalTo(separateLineView.snp.bottom).offset(14)
+            $0.leading.equalToSuperview().inset(15)
+            $0.trailing.equalToSuperview().inset(12)
+            $0.bottom.equalToSuperview().inset(10)
+        }
+        
+        nowWeatherView.snp.makeConstraints {
+            $0.leading.top.bottom.equalToSuperview()
+        }
+        
+        afterOneHourWeatherView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(nowWeatherView.snp.trailing).offset(22)
+        }
+        
+        afterTwoHourWeatherView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(afterOneHourWeatherView.snp.trailing).offset(22)
+        }
+        
+        afterThreeHourWeatherView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(afterTwoHourWeatherView.snp.trailing).offset(22)
+        }
+        
+        afterFourHourWeatherView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(afterThreeHourWeatherView.snp.trailing).offset(22)
+        }
+        
+        afterFiveHourWeatherView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(afterFourHourWeatherView.snp.trailing).offset(22)
+        }
+        
+        afterSixHourWeatherView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(afterFiveHourWeatherView.snp.trailing).offset(22)
+        }
+        
+        afterSevenHourWeatherView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(afterSixHourWeatherView.snp.trailing).offset(22)
+        }
+        
+        afterEightHourWeatherView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(afterSevenHourWeatherView.snp.trailing).offset(22)
+        }
+        
+        afterNineHourWeatherView.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview()
+            $0.leading.equalTo(afterEightHourWeatherView.snp.trailing).offset(22)
+            $0.trailing.equalToSuperview()
+        }
     }
 }
